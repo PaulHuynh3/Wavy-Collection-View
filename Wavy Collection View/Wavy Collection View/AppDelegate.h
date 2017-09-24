f89f3247be5bbfb81e21a264b863430bb4ad5bd0
Paul <paul.huynh3@gmail.com>
2017-09-23 22:05:24 -0400

    initial

---
+++ Wavy Collection View/Wavy Collection View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wavy Collection View
+//
+//  Created by Paul on 2017-09-23.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wavy Collection View/Wavy Collection View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wavy Collection View
+//
+//  Created by Paul on 2017-09-23.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
